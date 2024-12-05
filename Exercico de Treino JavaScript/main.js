250da3078e9061574d01c785f7b9f2ab336aa17f
Leonardo <leobortoli2005@gmail.com>
2024-12-05 20:10:55 -0300

    add main.js

--- Exercico de Treino JavaScript/main.js
+++ Exercico de Treino JavaScript/main.js
@@ -4,12 +4,12 @@ const numero2 = document.getElementById('numero2')
 const containerMensagemSucesso = document.querySelector('sucess-message')
 const containerMensagemErro = document.querySelector('error-message')
 
-function verificar (numero1, numero2) {
+function verify (numero1, numero2) {
     return numero1 < numero2
 }   
 
 form.addEventListener('submit', function (e){
     const mensagemSucesso = `Sucesso: O número 2 (${numero2.value}) é maior que o numéro 1(${numero1.value})!`
     const mensagemErro = `Erro: O número 1 (${numero1.value}): é maior que o número 2 (${numero2.value}). Inverta os números dos campos!`
-    formEValido = verificar (numero1.value, numero2.value)
+    formEValido = verify (numero1.value, numero2.value)
 })
