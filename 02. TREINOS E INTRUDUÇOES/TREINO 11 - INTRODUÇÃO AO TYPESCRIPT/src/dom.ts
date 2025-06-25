2c6c9fcff7b15b34bfddc7bf97ca1e6e0617887c
Leonardo <leobortoli2005@gmail.com>
2025-06-25 18:37:06 -0300

    finalização do modulo de TS

---
+++ 02. TREINOS E INTRUDUÇOES/TREINO 11 - INTRODUÇÃO AO TYPESCRIPT/src/dom.ts
@@ -0,0 +1,6 @@
+const campoNome = document.getElementById('name');
+const formulario = document.getElementsByTagName('form');
+const botaoEnviar = document.getElementById('btn-enviar');
+
+botaoEnviar?.addEventListener('click', (e: MouseEvent) => {
+})
